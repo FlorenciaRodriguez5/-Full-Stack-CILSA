e924484b5073419975fba1f49b8bc57fe6e567eb
Florencia Rodriguez <florr_tvp@hotmail.com>
2024-07-12 20:04:27 -0300

    Modificaciones en CSS/Agrego estilo incial/rearmado de validacion en js

--- Ejercicio 0 - 4/form.js
+++ Ejercicio 0 - 4/form.js
@@ -2,24 +2,34 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById("estilo1").addEventListener("click", function() {
-      document.getElementById("estilo").setAttribute("href", "form.css" );
+      document.getElementById("estilo").setAttribute("href", "./CSS/form.css" );
     });
   
     document.getElementById("contraste").addEventListener("click", function() {
-      document.getElementById("estilo").setAttribute("href", "contraste.css");
+      document.getElementById("estilo").setAttribute("href", "./CSS/contraste.css");
     });
 });
 
-  
+
 
 function validacion (){
  var email = document.getElementById("email").value;
 
  if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor)) ) {
+   alert['ERROR El valor ingresado debe cumplir la estructura gramatical de un correo electronico']
+   return false; 
+  }
+  else valor = document.getElementById("Nombre").value; 
+   if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+   return false;
+  }
+  else valor = document.getElementById("campo").value;
+   if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
    return false;
   }
  
 }
+
 
 
 
